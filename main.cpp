81373faeec232942ad4a48a6cb86707764ddb61c
Chambon Lea <lchambon@bs406-s16-19.insa-lyon.fr>
2018-05-07 17:27:28 +0200

    main modif 1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "Case.h"
+
 using namespace std;
 
 int main(){
@@ -6,7 +8,13 @@ int main(){
     cout<< "Hello World !" <<endl;
     cout<< " " <<endl;
 
+	Environnement environnement1 ();
+	Environnement environnement2 (25,7500,0.1,0);
+	
+	environnement.diffusion();
+	
     return 0;
+   
     
 }
 
